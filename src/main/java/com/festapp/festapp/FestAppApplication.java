7823282cc552a4eefbef53e924107421027fdf07
Hedge90 <kovacs.kristof90@gmail.com>
2023-09-05 13:52:13 +0200

    first commit

---
+++ src/main/java/com/festapp/festapp/FestAppApplication.java
@@ -0,0 +1,13 @@
+package com.festapp.festapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FestAppApplication.class, args);
+    }
+
+}
