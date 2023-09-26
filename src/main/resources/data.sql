01ec4e4b6ec5fb2f268425fb1e79e17b88e4e2aa
leocius1 <leocius@gmail.com>
2023-09-26 16:20:15 +0200

    SQL script before running the application

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+
+INSERT INTO days (id, date, name)
+VALUES (1, '2024-07-01','FRIDAY'),
+       (2, '2024-07-02', 'SATURDAY');
+
+INSERT INTO artists (id, name, date, bio, day_id)
+VALUES (1, 'Bob Marley','2024-07-01','Regi',1),
+       (2, 'Edda', '2024-07-02', 'urleny',2);
+
+INSERT INTO organizers (id, email, name, password)
+VALUES (1,'leo@gmail.hu','Leo','$2a$10$O7IEvhwRfIRE6dvB8FHH4.vvKpKDhLasn6a20ekzg0dbS.j3iu/tq'),
+       (2, 'kristof@gmail.hu', 'Kristof','$2a$10$SaPeFYZJS8QzSye/ySdf2uRIoFVwagRckQv2PeMnnC7xvmrH3x0Ym');
+
+
